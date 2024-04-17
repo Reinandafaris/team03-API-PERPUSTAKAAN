40c20e29c7616135f94a3f90befea5c5d402cb67
Reinandafaris <reinandafaris7@gmail.com>
2024-04-17 08:12:18 +0700

    Add login and register pages for authentication

--- controllers/allPagesController.js
+++ controllers/allPagesController.js
@@ -2,6 +2,16 @@ const HomePage = async (req, res) => {
   res.render("index.ejs");
 };
 
+const LoginPage = async (req, res) => {
+  res.render("components/auth/loginPage.ejs");
+};
+
+const RegisterPage = async (req, res) => {
+  res.render("components/auth/registerPage.ejs");
+};
+
 module.exports = {
   HomePage,
+  LoginPage,
+  RegisterPage,
 };
--- routes/allPagesRouter.js
+++ routes/allPagesRouter.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const pageController = require("../controllers/allPagesController");
 
 router.get("/", pageController.HomePage);
+router.get("/login", pageController.LoginPage);
+router.get("/register", pageController.RegisterPage);
 
 module.exports = router;
