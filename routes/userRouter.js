b51ebe3de6d76c07cfe081c62ac69b5a15516984
Reinandafaris <reinandafaris7@gmail.com>
2024-04-17 15:35:25 +0700

    Refactor userRouter.js to use destructured imports and middleware functions

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,45 @@
 const router = require("express").Router();
 
-const User = require("../controllers/userController");
+const { User } = require("../controllers/userController");
+
+const userController = require("../controllers/userController");
 
 const upload = require("../middlewares/uploader");
 const autentikasi = require("../middlewares/authenticate");
 const checkRole = require("../middlewares/checkRole");
+const checkOwnership = require("../middlewares/checkOwnership");
+const checkId = require("../middlewares/checkId");
 
-router.get("/", User.findUsers);
-router.get("/:id", User.findUserById);
-router.patch("/:id", User.updateUser);
-router.delete("/:id", User.deleteUser);
+router.post(
+  "/",
+  autentikasi,
+  checkRole("Admin, Manager"),
+  upload.array("images"),
+  userController.createUser
+);
+router.get("/", autentikasi, userController.findUsers);
+router.get(
+  "/:id",
+  autentikasi,
+  checkId(User),
+  userController.findUserById
+);
+router.patch(
+  "/:id",
+  autentikasi,
+  checkId(User),
+  checkRole(["Admin", "Manager"]),
+  checkOwnership,
+  upload.array("images"),
+  userController.updateUser
+);
+router.delete(
+  "/:id",
+  autentikasi,
+  checkId(User),
+  checkRole(["Admin", "Manager"]),
+  checkOwnership,
+  userController.deleteUser
+);
 
 module.exports = router;
