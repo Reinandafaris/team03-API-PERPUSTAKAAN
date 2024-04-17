e9a665a63c8a43de1c9c550ce3501a116466ecf0
Reinandafaris <reinandafaris7@gmail.com>
2024-04-17 15:22:23 +0700

    Add user router for handling user routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const AllPages = require("./allPagesRouter");
 const Auth = require("./authRouter");
 const Product = require("./productRouter");
 const Shop = require("./shopRouter");
+const User = require("./userRouter");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../docs/swagger.json");
 
@@ -18,5 +19,6 @@ router.use("/", AllPages);
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/products", Product);
 router.use("/api/v1/shops", Shop);
+router.use("/api/v1/users", User);
 
 module.exports = router;
