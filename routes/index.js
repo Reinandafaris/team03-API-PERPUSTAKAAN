5eabeb97ccbbe474831bb78f11d264a7bdf81afe
Reinandafaris <reinandafaris7@gmail.com>
2024-04-17 13:03:31 +0700

    Add product router for handling product routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const AllPages = require("./allPagesRouter");
 const Auth = require("./authRouter");
+const Product = require("./productRouter");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../docs/swagger.json");
 
@@ -14,5 +15,6 @@ router.use("/", AllPages);
 
 // route api
 router.use("/api/v1/auth", Auth);
+router.use("/api/v1/products", Product);
 
 module.exports = router;
